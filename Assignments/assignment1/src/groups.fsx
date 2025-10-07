765f9c3874180e606c1b7ae0bf9f5dd154d990a1
klombo <klementottosen@gmail.com>
2025-10-07 11:26:37 +0200

    assignment resub changes

--- Assignments/assignment1/src/groups.fsx
+++ Assignments/assignment1/src/groups.fsx
@@ -1,15 +1,15 @@
 let rec createPairs (students : int list) =
     match students with
-    | first :: second :: tail -> (first,second) :: createPairs tail
+    | first :: second :: tail :: [] -> [[first;second;tail]]
+    | first :: second :: tail -> [[first;second]] @ createPairs tail
     | _ -> []
 
 let createLineList (students : int list) (lineNum : int) =
     List.filter (fun studentID -> studentID % 4 = lineNum) students;;
 
-let studentList = [0..99]
+let studentList = [1..100]
 
 createPairs studentList |> printfn "%A"
-
 
 createLineList studentList 0 |> createPairs |> printfn "Line 0 Groups: %A"
 createLineList studentList 1 |> createPairs |> printfn "Line 1 Groups: %A"
