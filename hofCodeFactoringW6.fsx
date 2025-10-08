b5d19fbfca1d1d4650ede2181c4004bd3ae63baf
klombo <klementottosen@gmail.com>
2025-10-08 22:09:00 +0200

    Deliberate Practic Week6

--- hofCodeFactoringW6.fsx
+++ hofCodeFactoringW6.fsx
@@ -17,12 +17,10 @@ let rec zipAdd (xInputList: int list) (yInputList: int list):int list =
     | [], y1::yn -> (0 + y1) :: zipAdd [] yn
     | [], [] -> []
 
-let rec zipWith (f : 'a -> 'b -> 'c) (xInputList : 'a list) (yInputList : 'a list): 'c list =
+let rec zipWith (f : 'a -> 'b -> 'c) (xInputList : 'a list) (yInputList : 'a list) : 'c list =
     match xInputList, yInputList with
     | x1::xn, y1::yn -> f x1 y1 :: zipWith f xn yn
-    | x1::xn, [] -> f x1 [] :: zipWith f xn []
-    | [], y1::yn -> f [] y1 :: zipWith f [] yn
-    | [], [] -> []
+    | _ -> []
 
 (*let rec printAll (f : 'a -> string) (inputList : 'a list): unit =
     match inputList with 
@@ -31,8 +29,9 @@ let rec zipWith (f : 'a -> 'b -> 'c) (xInputList : 'a list) (yInputList : 'a lis
     | [] -> []*)
 
 let exampleList = [1; 0; -3; 15; 2; 3; 4; -5; 7]
-let exampleList2 = [20; -4; 12; -2; 0; 11; 1; 3; -4]
+let exampleList2 = [20; -4; 12; -2; 0; 11; 1; 3; -4; 9]
 
 filterPos exampleList |> printfn "filterPos: %A"
 addPos exampleList |> printfn "addPos: %A"
 zipAdd exampleList exampleList2 |> printfn "zipAdd: %A"
+zipWith (fun x y -> x * y) exampleList exampleList2 |> printfn "zipWith: %A"
