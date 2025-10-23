10550c9714c88363e2e5030e3247925f786ebc20
klombo <klementottosen@gmail.com>
2025-10-23 22:52:06 +0200

    Assignment 2 pascal solved

--- Assignments/assignment2/src/Pascal/Pascal.fs
+++ Assignments/assignment2/src/Pascal/Pascal.fs
@@ -2,15 +2,15 @@ module Pascal
 
 let rec pascal ((n, k) : int * int) : int =
     match (n, k) with
+    | (n, k) when k > n || n < 0 || k < 0 -> 0
     | (_, 0) -> 1
     | (n, k) when n = k -> 1
-    | (n, k) when k > n -> 0
     | (n, k) -> pascal (n-1, k-1) + pascal (n-1,k)
 
 let pascalNoRec ((n, k) : int * int) : int =
-    if n <= 0 || k < 0 || k > n then 0
-    else // Pascals triangle is initilazed at zero therefore it is n + 
-        let mutable triangle = Array.init (n+1) (fun index -> Array.create (index+1) 0)
+    if n < 0 || k < 0 || k > n then 0
+    else // Pascals triangle is initilazed at zero therefore it is n + 1 
+        let mutable triangle = Array.init (n+1) (fun index -> Array.zeroCreate (index+1))
         for row in 0..n do
             for column in 0..row do
                 if column = 0 || column = row then 
--- Assignments/assignment2/src/Pascal/PascalTest.fsx
+++ Assignments/assignment2/src/Pascal/PascalTest.fsx
@@ -1,4 +1,62 @@
 #load "Pascal.fs"
+#load "Pascal.fsi"
 open Pascal
 
-printfn "%A" (pascalNoRec (5, 2))
+let test (condition: bool): string =
+    if condition then "[PASSED]"
+    else "[FAILED]"
+
+// Property 1 Testing
+
+let hasPropertyOne ((n,k) : int * int) : bool =
+    if (k = 0 || k = n) then
+        pascal (n,k) = 1
+    else false
+
+let hasPropertyOneNoRec ((n,k) : int * int) : bool =
+    if (k = 0 || k = n) then
+        pascalNoRec (n,k) = 1
+    else false
+
+// Property 2 Testing
+let hasPropertyTwo ((n,k) : int * int) : bool =
+    if (n > k && k > 0) then
+        pascal(n,k) = pascal(n-1, k-1) + pascal(n-1,k)
+    else false
+
+let hasPropertyTwoNoRec ((n,k) : int * int) : bool = 
+    if (n > k && k > 0) then
+        pascalNoRec(n,k) = pascalNoRec(n-1,k-1) + pascalNoRec(n-1,k)
+    else false
+
+let inputOutOfTriangle ((n,k) : int * int) : bool =
+    if (n < 0 || k<0 || k>n) then
+        pascal (n,k) = 0
+    else false
+
+let inputOutOfTriangleNoRec ((n,k): int * int) : bool =
+    if (n < 0 || k<0 || k>n) then
+        pascalNoRec (n,k) = 0
+    else false
+
+
+// Testing Property 1
+printfn "!!! Testing Property 1 !!!"
+let propertyOneTestValues = [ (0,0); (6,0); (6,6) ]
+for pair in propertyOneTestValues do
+    printfn "Property 1 Test for pascal with (n, k): %A = %s" (pair) (test (hasPropertyOne pair))
+    printfn "Property 1 Test for pascalNoRec with (n, k): %A = %s" (pair) (test (hasPropertyOneNoRec pair))
+
+// Testing Property 2
+printfn "!!! Testing Property 2 !!!"
+let propertyTwoTestValues = [ (5,2); (6,3); (2,1) ]
+for pair in propertyTwoTestValues do
+    printfn "Property 2 Test for pascal with (n, k): %A = %s" (pair) (test (hasPropertyTwo pair))
+    printfn "Property 2 Test for pascalNoRec with (n, k): %A = %s" (pair) (test (hasPropertyTwoNoRec pair))
+
+// Testing for cases outside pascals Triangle
+printfn "!!! Testing cases outside pascals Triangle !!!"
+let invalidTestValues = [ (-1, 0); (3, -1); (3,5)]
+for pair in invalidTestValues do
+    printfn "Test that Inputs out of Triangle return 0 for pascal with (n, k): %A = %s" (pair) (test (inputOutOfTriangle pair))
+    printfn "Test that Inputs out of Triangle return 0 for pascalNoRec with (n, k): %A = %s" (pair) (test (inputOutOfTriangleNoRec pair))
