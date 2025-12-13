9154cf6d4bffb7111bba62925c2b9fce6096e33e
klombo <klementottosen@gmail.com>
2025-12-13 16:58:49 +0100

    assignment3 turn in

--- Assignments/assignment3/src/meetUp/MeetUpExamples.fsx
+++ Assignments/assignment3/src/meetUp/MeetUpExamples.fsx
@@ -3,7 +3,7 @@
 
 open MeetUp
 
-let exampleList: Participant list = [
+let exampleList: Participant List = [
     Normal("David Attenborough", 0.00, 0.98, 0.61);
     CopyCat("Linda Hamilton",0.36, 0.44, 0.25);
     Skeptic("Jesus Christus", 0.25, 0.72, 0.82);
@@ -16,34 +16,10 @@ let exampleList: Participant list = [
     Skeptic("Charlie Kirk", 1.0, 0.09, 1.0)
 ]
 
-let onlyNormalList: Participant list = [
-    Normal("David Attenborough", 0.00, 0.98, 0.61);
-    Normal("Linda Hamilton",0.36, 0.44, 0.25);
-    Normal("Jesus Christus", 0.25, 0.72, 0.82);
-    Normal("Thomas Tog", 0.15, 0.65, 0.09);
-    Normal("Jessica Rabbit", 0.52, 0.14, 0.15);
-    Normal("Christopher Columbus", 0.65, 0.55, 0.42);
-    Normal("Elizabeth Turner", 0.5, 0.35, 0.57);
-    Normal("Dan Turrell", 0.78, 0.81, 0.78);
-    Normal("Queen Elizabeth", 0.92, 0.52, 0.51);
-    Normal("Charlie Kirk", 1.0, 0.09, 1.0)
-]
-
-let extremeList: Participant list = [
-    Normal("David Attenborough", 0.98, 0.98, 0.61);
-    CopyCat("Linda Hamilton",0.95, 0.44, 0.25);
-    Skeptic("Jesus Christus", 0.1, 0.72, 0.82);
-    Normal("Thomas Tog", 0.05, 0.65, 0.09);
-    CopyCat("Jessica Rabbit", 1, 0.14, 0.15);
-    Normal("Christopher Columbus", 0.0, 0.55, 0.42);
-    Skeptic("Elizabeth Turner", 0.1, 0.35, 0.57);
-    Normal("Dan Turrell", 0.99, 0.81, 0.78);
-    Normal("Queen Elizabeth", 0.92, 0.52, 0.51);
-    Skeptic("Charlie Kirk", 1.0, 0.09, 1.0)
-]
 
 
-let SimulationWithNoVisual (listOfParticipants: Participant List) =
+let SimulationWithNoVisual (listOfParticipants: Participant List) (amountOfRounds:int) =
+    printfn "\n"
     let getAveragePoliticalView (politicalViews: Participant List): float = 
         politicalViews 
         |> List.map (fun person -> match person with
@@ -56,20 +32,27 @@ let SimulationWithNoVisual (listOfParticipants: Participant List) =
 
     printPoliticalView listOfParticipants
 
-    for i in 1..100000000 do
+    for i in 1..amountOfRounds do
         simulateMeeting listOfParticipants |> ignore
 
     printPoliticalView listOfParticipants
 
+let testTypes =
+    let testList: Participant List = [
+        Normal("Normal", 1.0, 0.2, 0.5)
+        CopyCat ("CopyCat", 0.6, 0.2, 0.5)
+        Skeptic ("Skeptic", 0.2,0.2,0.5)
+    ]
+    for i in 0..testList.Length-1 do
+        for j in 0..testList.Length-1 do
+            let isAMatch = testList[i].MatchScore (testList[j]) > 0.5
+            testList[i].OnInteraction (testList[j]) isAMatch
+            |> printfn "when %s with politicalView: %A, meets %s with politicalView: %A, then amount moved: %A." 
+                testList[i].name testList[i].politicalView testList[j].name testList[j].politicalView 
 
-// runSimulation exampleList
-SimulationWithNoVisual exampleList
-SimulationWithNoVisual exampleList
-SimulationWithNoVisual exampleList
+testTypes
+SimulationWithNoVisual exampleList 10000
+runSimulation exampleList
 
 
-// runSimulation onlyNormalList
-// SimulationWithNoVisual onlyNormalList
 
-// runSimulation extremeList
-// SimulationWithNoVisual extremeList
--- Assignments/assignment3/src/meetUp/meetUp.fs
+++ Assignments/assignment3/src/meetUp/meetUp.fs
@@ -258,7 +258,6 @@ let buildPicture (state: SimulationState) (midPoint : float * float) : Picture =
 
 // Funktion der håndtere hvert TimerTick, interval, kører simulateMeeting med participants liste.
 let react (state: SimulationState) (ev: Event) : SimulationState option =
-    let comps = connectedComponents (createGraph state.Participants (createEdgeMatrix state.Participants))
     match ev with
     | Event.TimerTick ->
         let p1, p2, isMatch = simulateMeeting state.Participants
@@ -276,7 +275,7 @@ let react (state: SimulationState) (ev: Event) : SimulationState option =
 let runSimulation(participantList: Participant List)=
     let width = 700
     let height = 700
-    let interval = (Some 10000)
+    let interval = (Some 250)
     let midPoint = float width * 0.5, float height * 0.5
 
     let initialState : SimulationState =
